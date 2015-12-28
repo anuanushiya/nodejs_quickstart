3964c0eb0078f82449bef6fdd960c1ccc351abcf
CookingRobot <hi@weise.io>
2015-12-28 23:48:07 +0100

    console message for server start

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ var app = express();
 var swig = require('swig');
 var consolidate = require('consolidate');
 
+var port = 1289;
+
 app.set('view engine', 'html');
 app.set('views', __dirname + '/templates');
 app.engine('.html', consolidate.swig);
 app.use("/static", express.static(__dirname + '/static'));
 
-app.listen(process.env.PORT || 1289);
+app.listen(process.env.PORT || port, function() {
+    console.log("[DONE] Server startet at port "+port);
+});
 
 var controllers = require('./controllers');
 controllers.set(app);
